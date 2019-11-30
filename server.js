f96a72f033ae616add908e1c9770ce87603e924c
Joe O <j0liverko@gmail.com>
2019-11-29 21:59:54 -0600

    Added crud routes for bootcamps

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const dotenv = require('dotenv')
 dotenv.config({ path: './config/config.env' })
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('hello from express')
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'show all bootcamps' })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `show single bootcamp ${req.params.id}` })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create new bootcamp' })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `update single bootcamp ${req.params.id}` })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `delete single bootcamp ${req.params.id}` })
 })
 const PORT = process.env.PORT || 5000
 
